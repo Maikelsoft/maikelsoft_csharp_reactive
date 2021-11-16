124a48ea6a5b88ee018f24a075a6e90a85a29e56
Maikel de Laat <devtools@maikeldelaat.nl>
2021-11-16 19:08:49 +0100

    no message

--- Maikelsoft.Reactive/ExtendIObservable.cs
+++ Maikelsoft.Reactive/ExtendIObservable.cs
@@ -20,10 +20,8 @@ namespace Maikelsoft.Reactive
 		[Pure]
 		public static IObservable<ITry<T>> Where<T>(this IObservable<ITry<T>> source,
 			Func<T, bool> predicate)
-			where T : IEquatable<T>
-		{
-			return source.Where(@try => @try.HasError || predicate(@try.Value));
-		}
+			where T : IEquatable<T> =>
+			source.Where(x => x.HasError || predicate(x.Value));
 
 		/// <summary>
 		/// 
@@ -31,15 +29,30 @@ namespace Maikelsoft.Reactive
 		/// <typeparam name="TSource"></typeparam>
 		/// <typeparam name="T"></typeparam>
 		/// <param name="source"></param>
-		/// <param name="selector"></param>
+		/// <param name="selectorThatCanThrowException"></param>
 		/// <returns></returns>
 		[Pure]
-		public static IObservable<ITry<T>> Select<TSource, T>(
-			this IObservable<ITry<TSource>> source, Func<TSource, T> selector)
+		public static IObservable<ITry<T>> TrySelect<TSource, T>(
+			this IObservable<TSource> source, Func<TSource, T> selectorThatCanThrowException)
+			where TSource : IEquatable<TSource>
+			where T : IEquatable<T> =>
+			source.Select(x => x.TrySelect(selectorThatCanThrowException));
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <typeparam name="TSource"></typeparam>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="source"></param>
+		/// <param name="selectorThatCanThrowException"></param>
+		/// <returns></returns>
+		[Pure]
+		public static IObservable<ITry<T>> TrySelect<TSource, T>(
+			this IObservable<ITry<TSource>> source, Func<TSource, T> selectorThatCanThrowException)
 			where TSource : IEquatable<TSource>
 			where T : IEquatable<T>
 		{
-			return source.Select(@try => @try.Select(selector));
+			return source.Select(x => x.TrySelect(selectorThatCanThrowException));
 		}
 
 		/// <summary>
@@ -51,86 +64,12 @@ namespace Maikelsoft.Reactive
 		/// <param name="selector"></param>
 		/// <returns></returns>
 		[Pure]
-		public static IObservable<ITry<T>> Select<TSource, T>(
+		public static IObservable<ITry<T>> BindSelect<TSource, T>(
 			this IObservable<ITry<TSource>> source, Func<TSource, ITry<T>> selector)
 			where TSource : IEquatable<TSource>
 			where T : IEquatable<T>
 		{
 			return source.Select(@try => @try.Bind(selector));
 		}
-
-		///// <summary>
-		///// 
-		///// </summary>
-		///// <typeparam name="T"></typeparam>
-		///// <param name="source"></param>
-		///// <param name="onNextValue"></param>
-		///// <param name="onNextError"></param>
-		///// <param name="cancellationToken"></param>
-		//public static void Subscribe<T>(this IObservable<ITry<T>> source, Action<T> onNextValue,
-		//	Action<string> onNextError, CancellationToken cancellationToken)
-		//	where T : IEquatable<T>
-		//{
-		//	source.Subscribe(@try =>
-		//	{
-		//		@try.Match(onNextError, onNextValue);
-		//	}, cancellationToken);
-		//}
-
-		///// <summary>
-		///// 
-		///// </summary>
-		///// <typeparam name="T"></typeparam>
-		///// <param name="source"></param>
-		///// <param name="onNextValue"></param>
-		///// <param name="onNextError"></param>
-		///// <param name="onCompleted"></param>
-		///// <param name="cancellationToken"></param>
-		//public static void Subscribe<T>(this IObservable<ITry<T>> source, Action<T> onNextValue,
-		//	Action<string> onNextError, Action onCompleted, CancellationToken cancellationToken)
-		//	where T : IEquatable<T>
-		//{
-		//	source.Subscribe(@try =>
-		//	{
-		//		@try.Match(onNextError, onNextValue);
-		//	}, onCompleted, cancellationToken);
-		//}
-
-		///// <summary>
-		///// 
-		///// </summary>
-		///// <typeparam name="T"></typeparam>
-		///// <param name="source"></param>
-		///// <param name="onNextValue"></param>
-		///// <param name="onNextError"></param>
-		///// <returns></returns>
-		//public static IDisposable Subscribe<T>(this IObservable<ITry<T>> source,
-		//	Action<T> onNextValue, Action<string> onNextError)
-		//	where T : IEquatable<T>
-		//{
-		//	return source.Subscribe(@try =>
-		//	{
-		//		@try.Match(onNextError, onNextValue);
-		//	});
-		//}
-
-		///// <summary>
-		///// 
-		///// </summary>
-		///// <typeparam name="T"></typeparam>
-		///// <param name="source"></param>
-		///// <param name="onNextValue"></param>
-		///// <param name="onNextError"></param>
-		///// <param name="onCompleted"></param>
-		///// <returns></returns>
-		//public static IDisposable Subscribe<T>(this IObservable<ITry<T>> source,
-		//	Action<T> onNextValue, Action<string> onNextError, Action onCompleted)
-		//	where T : IEquatable<T>
-		//{
-		//	return source.Subscribe(@try =>
-		//	{
-		//		@try.Match(onNextError, onNextValue);
-		//	}, onCompleted);
-		//}
 	}
 }
