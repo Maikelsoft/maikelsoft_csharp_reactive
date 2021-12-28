8364a7ea4f4362037f5f2a74876bef1767f3aeda
Maikel de Laat <devtools@maikeldelaat.nl>
2021-12-28 18:16:14 +0100

    no message

--- nuget_pack.bat
+++ nuget_pack.bat
@@ -1,11 +1,11 @@
 @echo off
 
 echo Creating directories...
-mkdir NuGet\lib\net5.0
+mkdir NuGet\lib\net6.0
 
 echo Copying .NET libraries...
-copy Maikelsoft.Reactive\bin\Release\net5.0\*.dll NuGet\lib\net5.0\
-copy Maikelsoft.Reactive\bin\Release\net5.0\*.xml NuGet\lib\net5.0\
+copy Maikelsoft.Reactive\bin\Release\net6.0\*.dll NuGet\lib\net6.0\
+copy Maikelsoft.Reactive\bin\Release\net6.0\*.xml NuGet\lib\net6.0\
 
 echo Building NuGet package...
 NuGet.exe pack NuGet\Maikelsoft.Reactive.nuspec -OutputDirectory C:\Development\nuget_packages
