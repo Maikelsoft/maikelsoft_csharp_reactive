7b40031756821356c3c6e00575cabe06cd523650
Maikel de Laat <devtools@maikeldelaat.nl>
2022-02-20 14:55:06 +0100

    no message

--- Maikelsoft.Reactive/ExtendIObservable.cs
+++ Maikelsoft.Reactive/ExtendIObservable.cs
@@ -5,192 +5,176 @@ using Maikelsoft.Monads;
 
 namespace Maikelsoft.Reactive
 {
-	/// <summary>
-	/// Extends the <see cref="IObservable{T}"/> interface.
-	/// </summary>
-	public static class ExtendIObservable
-	{
-		#region Try Monad
+    /// <summary>
+    /// Extends the <see cref="IObservable{T}"/> interface.
+    /// </summary>
+    public static class ExtendIObservable
+    {
+        #region Either Monad
+        
+        [Pure]
+        public static IObservable<TLeft> LeftValues<TLeft, TRight>(this IObservable<Either<TLeft, TRight>> source)
+            where TLeft : notnull
+            where TRight : notnull
+        {
+            return source.Where(@try => @try.HasLeft).Select(either => either.Left);
+        }
 
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <typeparam name="TSource"></typeparam>
-		/// <typeparam name="T"></typeparam>
-		/// <param name="source"></param>
-		/// <param name="selector"></param>
-		/// <returns></returns>
-		[Pure]
-		public static IObservable<Try<T>> TrySelect<TSource, T>(
-			this IObservable<Try<TSource>> source, Func<TSource, T> selector)
-			where TSource : notnull
-			where T : notnull =>
-			source.Select(@try => @try.Select(selector));
+        [Pure]
+        public static IObservable<TRight> RightValues<TLeft, TRight>(this IObservable<Either<TLeft, TRight>> source)
+            where TLeft : notnull
+            where TRight : notnull
+        {
+            return source.Where(@try => @try.HasRight).Select(either => either.Right);
+        }
 
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <typeparam name="TSource"></typeparam>
-		/// <typeparam name="TResult"></typeparam>
-		/// <param name="source"></param>
-		/// <param name="selector"></param>
-		/// <returns></returns>
-		[Pure]
-		public static IObservable<Try<TResult>> TrySelect<TSource, TResult>(
-			this IObservable<Try<TSource>> source, Func<TSource, int, TResult> selector)
-			where TSource : notnull
-			where TResult : notnull =>
-			source.Select((@try, i) => @try.Select(value => selector(value, i)));
+        [Pure]
+        public static IObservable<Either<TResult, TRight>> SelectLeft<TLeft, TRight, TResult>(
+            this IObservable<Either<TLeft, TRight>> source, Func<TLeft, TResult> selector)
+            where TLeft : notnull
+            where TRight : notnull
+            where TResult : notnull
+        {
+            return source.Select(@try => @try.SelectLeft(selector));
+        }
 
+        [Pure]
+        public static IObservable<Either<TLeft, TResult>> SelectRight<TLeft, TRight, TResult>(
+            this IObservable<Either<TLeft, TRight>> source, Func<TRight, TResult> selector)
+            where TLeft : notnull
+            where TRight : notnull
+            where TResult : notnull
+        {
+            return source.Select(@try => @try.SelectRight(selector));
+        }
 
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <typeparam name="TSource1"></typeparam>
-		/// <typeparam name="TSource2"></typeparam>
-		/// <typeparam name="TResult"></typeparam>
-		/// <param name="source1"></param>
-		/// <param name="source2"></param>
-		/// <param name="resultSelector"></param>
-		/// <returns></returns>
-		[Pure]
-		public static IObservable<Try<TResult>> TryCombineLatest<TSource1, TSource2, TResult>(
-			this IObservable<Try<TSource1>> source1,
-			IObservable<Try<TSource2>> source2, Func<TSource1, TSource2, TResult> resultSelector)
-			where TSource1 : notnull
-			where TSource2 : notnull
-			where TResult : notnull =>
-			source1.CombineLatest(source2, (try1, try2) => try1.CombineWith(try2, resultSelector));
+        #endregion
+        
+        #region Try Monad
 
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <typeparam name="TSource1"></typeparam>
-		/// <typeparam name="TSource2"></typeparam>
-		/// <typeparam name="TResult"></typeparam>
-		/// <param name="source1"></param>
-		/// <param name="source2"></param>
-		/// <param name="resultSelector"></param>
-		/// <returns></returns>
-		[Pure]
-		public static IObservable<Try<TResult>> TryWithLatestFrom<TSource1, TSource2, TResult>(
-			this IObservable<Try<TSource1>> source1,
-			IObservable<Try<TSource2>> source2, Func<TSource1, TSource2, TResult> resultSelector)
-			where TSource1 : notnull
-			where TSource2 : notnull
-			where TResult : notnull =>
-			source1.WithLatestFrom(source2, (try1, try2) => try1.CombineWith(try2, resultSelector));
+        [Pure]
+        public static IObservable<T> Values<T>(this IObservable<Try<T>> source)
+            where T : notnull
+        {
+            return source.Where(@try => @try.HasValue).Select(@try => @try.Value);
+        }
 
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <typeparam name="TSource1"></typeparam>
-		/// <typeparam name="TSource2"></typeparam>
-		/// <typeparam name="TResult"></typeparam>
-		/// <param name="source1"></param>
-		/// <param name="source2"></param>
-		/// <param name="resultSelector"></param>
-		/// <returns></returns>
-		[Pure]
-		public static IObservable<Try<TResult>> TryZip<TSource1, TSource2, TResult>(
-			this IObservable<Try<TSource1>> source1,
-			IObservable<Try<TSource2>> source2, Func<TSource1, TSource2, TResult> resultSelector)
-			where TSource1 : notnull
-			where TSource2 : notnull
-			where TResult : notnull =>
-			source1.Zip(source2, (try1, try2) => try1.CombineWith(try2, resultSelector));
+        [Pure]
+        public static IObservable<Error> Errors<T>(this IObservable<Try<T>> source)
+            where T : notnull
+        {
+            return source.Where(@try => @try.HasError).Select(@try => @try.Error);
+        }
 
-		#endregion
+        [Pure]
+        public static IObservable<Try<T>> Select<TSource, T>(
+            this IObservable<Try<TSource>> source, Func<TSource, T> selector)
+            where TSource : notnull
+            where T : notnull
+        {
+            return source.Select(@try => @try.Select(selector));
+        }
 
-		#region Optional Monad
+        [Obsolete("Use Select override")]
+        [Pure]
+        public static IObservable<Try<T>> TrySelect<TSource, T>(
+            this IObservable<Try<TSource>> source, Func<TSource, T> selector)
+            where TSource : notnull
+            where T : notnull
+        {
+            return source.Select(@try => @try.Select(selector));
+        }
 
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <typeparam name="TSource"></typeparam>
-		/// <typeparam name="T"></typeparam>
-		/// <param name="source"></param>
-		/// <param name="selector"></param>
-		/// <returns></returns>
-		[Pure]
-		public static IObservable<Optional<T>> OptionalSelect<TSource, T>(
-			this IObservable<Optional<TSource>> source, Func<TSource, T> selector)
-			where TSource : notnull
-			where T : notnull =>
-			source.Select(optional => optional.Select(selector));
+        //      [Pure]
+        //public static IObservable<Try<TResult>> TryCombineLatest<TSource1, TSource2, TResult>(
+        //	this IObservable<Try<TSource1>> source1,
+        //	IObservable<Try<TSource2>> source2, Func<TSource1, TSource2, TResult> resultSelector)
+        //	where TSource1 : notnull
+        //	where TSource2 : notnull
+        //	where TResult : notnull
+        //      {
+        //          return source1.CombineLatest(source2, (try1, try2) => try1.CombineWith(try2, resultSelector));
+        //      }
 
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <typeparam name="TSource"></typeparam>
-		/// <typeparam name="TResult"></typeparam>
-		/// <param name="source"></param>
-		/// <param name="selector"></param>
-		/// <returns></returns>
-		[Pure]
-		public static IObservable<Optional<TResult>> OptionalSelect<TSource, TResult>(
-			this IObservable<Optional<TSource>> source, Func<TSource, int, TResult> selector)
-			where TSource : notnull
-			where TResult : notnull =>
-			source.Select((optional, i) => optional.Select(value => selector(value, i)));
+        //      [Pure]
+        //public static IObservable<Try<TResult>> TryWithLatestFrom<TSource1, TSource2, TResult>(
+        //	this IObservable<Try<TSource1>> source1,
+        //	IObservable<Try<TSource2>> source2, Func<TSource1, TSource2, TResult> resultSelector)
+        //	where TSource1 : notnull
+        //	where TSource2 : notnull
+        //	where TResult : notnull
+        //      {
+        //          return source1.WithLatestFrom(source2, (try1, try2) => try1.CombineWith(try2, resultSelector));
+        //      }
 
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <typeparam name="TSource1"></typeparam>
-		/// <typeparam name="TSource2"></typeparam>
-		/// <typeparam name="TResult"></typeparam>
-		/// <param name="source1"></param>
-		/// <param name="source2"></param>
-		/// <param name="resultSelector"></param>
-		/// <returns></returns>
-		[Pure]
-		public static IObservable<Optional<TResult>> OptionalCombineLatest<TSource1, TSource2, TResult>(
-			this IObservable<Optional<TSource1>> source1,
-			IObservable<Optional<TSource2>> source2, Func<TSource1, TSource2, TResult> resultSelector)
-			where TSource1 : notnull
-			where TSource2 : notnull
-			where TResult : notnull =>
-			source1.CombineLatest(source2, (optional1, optional2) => optional1.CombineWith(optional2, resultSelector));
+        //      [Pure]
+        //public static IObservable<Try<TResult>> TryZip<TSource1, TSource2, TResult>(
+        //	this IObservable<Try<TSource1>> source1,
+        //	IObservable<Try<TSource2>> source2, Func<TSource1, TSource2, TResult> resultSelector)
+        //	where TSource1 : notnull
+        //	where TSource2 : notnull
+        //	where TResult : notnull
+        //      {
+        //          return source1.Zip(source2, (try1, try2) => try1.CombineWith(try2, resultSelector));
+        //      }
 
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <typeparam name="TSource1"></typeparam>
-		/// <typeparam name="TSource2"></typeparam>
-		/// <typeparam name="TResult"></typeparam>
-		/// <param name="source1"></param>
-		/// <param name="source2"></param>
-		/// <param name="resultSelector"></param>
-		/// <returns></returns>
-		[Pure]
-		public static IObservable<Optional<TResult>> OptionalWithLatestFrom<TSource1, TSource2, TResult>(
-			this IObservable<Optional<TSource1>> source1,
-			IObservable<Optional<TSource2>> source2, Func<TSource1, TSource2, TResult> resultSelector)
-			where TSource1 : notnull
-			where TSource2 : notnull
-			where TResult : notnull =>
-			source1.WithLatestFrom(source2, (optional1, optional2) => optional1.CombineWith(optional2, resultSelector));
+        #endregion
 
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <typeparam name="TSource1"></typeparam>
-		/// <typeparam name="TSource2"></typeparam>
-		/// <typeparam name="TResult"></typeparam>
-		/// <param name="source1"></param>
-		/// <param name="source2"></param>
-		/// <param name="resultSelector"></param>
-		/// <returns></returns>
-		[Pure]
-		public static IObservable<Optional<TResult>> OptionalZip<TSource1, TSource2, TResult>(
-			this IObservable<Optional<TSource1>> source1,
-			IObservable<Optional<TSource2>> source2, Func<TSource1, TSource2, TResult> resultSelector)
-			where TSource1 : notnull
-			where TSource2 : notnull
-			where TResult : notnull =>
-			source1.Zip(source2, (optional1, optional2) => optional1.CombineWith(optional2, resultSelector));
+        #region Optional Monad
 
-		#endregion
-	}
+        [Pure]
+        public static IObservable<Optional<T>> OptionalSelect<TSource, T>(
+            this IObservable<Optional<TSource>> source, Func<TSource, T> selector)
+            where TSource : notnull
+            where T : notnull
+        {
+            return source.Select(optional => optional.Select(selector));
+        }
+
+        [Pure]
+        public static IObservable<Optional<TResult>> OptionalSelect<TSource, TResult>(
+            this IObservable<Optional<TSource>> source, Func<TSource, int, TResult> selector)
+            where TSource : notnull
+            where TResult : notnull
+        {
+            return source.Select((optional, i) => optional.Select(value => selector(value, i)));
+        }
+
+        [Pure]
+        public static IObservable<Optional<TResult>> OptionalCombineLatest<TSource1, TSource2, TResult>(
+            this IObservable<Optional<TSource1>> source1,
+            IObservable<Optional<TSource2>> source2, Func<TSource1, TSource2, TResult> resultSelector)
+            where TSource1 : notnull
+            where TSource2 : notnull
+            where TResult : notnull
+        {
+            return source1.CombineLatest(source2,
+                (optional1, optional2) => optional1.CombineWith(optional2, resultSelector));
+        }
+
+        [Pure]
+        public static IObservable<Optional<TResult>> OptionalWithLatestFrom<TSource1, TSource2, TResult>(
+            this IObservable<Optional<TSource1>> source1,
+            IObservable<Optional<TSource2>> source2, Func<TSource1, TSource2, TResult> resultSelector)
+            where TSource1 : notnull
+            where TSource2 : notnull
+            where TResult : notnull
+        {
+            return source1.WithLatestFrom(source2,
+                (optional1, optional2) => optional1.CombineWith(optional2, resultSelector));
+        }
+
+        [Pure]
+        public static IObservable<Optional<TResult>> OptionalZip<TSource1, TSource2, TResult>(
+            this IObservable<Optional<TSource1>> source1,
+            IObservable<Optional<TSource2>> source2, Func<TSource1, TSource2, TResult> resultSelector)
+            where TSource1 : notnull
+            where TSource2 : notnull
+            where TResult : notnull
+        {
+            return source1.Zip(source2, (optional1, optional2) => optional1.CombineWith(optional2, resultSelector));
+        }
+
+        #endregion
+    }
 }
